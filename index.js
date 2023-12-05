4c5b6aafbbc73107553245389a6ea9d0f5c94126
passionate-code <60539975+passionate-code@users.noreply.github.com>
2023-12-05 09:26:44 +0800

    #19

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ app.listen(port, () => { //
   console.log("Server Running at http://localhost:3001/");
 });
 
+var get_updated_data = async (Model) => {
+  var docs = await Model.find({}) // get documents from sitesmodel
+  var json_docs = [];// array to store json objects that contains data for all sites
+  for (doc of docs) { // iterate docs
+    json_doc = {"id":doc._id,"name":doc.name,"url":doc.url,"statuscode":doc.statuscode,"date":doc.date} // define json object that contains data from each doc
+    json_docs.push(json_doc); // append array to array that store json objects that contains data for all sites
+  }
+  return json_docs; // return promise
+}
+
+app.get("/", async (request, response) => {
+  var json_docs = await get_updated_data(SitesModel); // execute get_updated_data func and assign array of json objects that contain updated data from mongo db
+  res.json(json_docs); // send array of json objects as a response
+});
+
 check_sites_scheduler(names_urls,SitesModel); 
 
 module.exports = app;
